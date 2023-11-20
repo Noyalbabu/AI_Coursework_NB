64fb354a1d118d7e475595e3d0d349f6f82bb322
Noyalbabu <20209749@hope.ac.uk>
2023-11-20 13:09:01 +0000

    Fake news detection using LR, DT, GB, RF

--- fake_news.py
+++ fake_news.py
@@ -7,45 +7,6 @@ from sklearn.metrics import accuracy_score, classification_report
 import re
 import string 
 
-# import the fake and real news datasets
-data_fake = pd.read_csv('Fake.csv')
-data_true = pd.read_csv('True.csv')
-
-data_fake.head()
-data_true.head()
-
-# Add a target class column to indicate whether the news is real or fake
-data_fake["class"] = 0 
-data_true["class"] = 1
-
-#Remove last 10 rows for manual testing 
-data_fake_manual_testing = data_fake.tail(10)
-for i in range(23480,23470,-1):
-    data_fake.drop([i], axis = 0, inplace = True)
-
-data_true_manual_testing = data_true.tail(10)
-for i in range(21416,21406,-1):
-    data_true.drop([i], axis = 0, inplace = True)
-
-data_fake_manual_testing['class'] = 0
-data_true_manual_testing['class'] = 1
-
-#Output the manual testing data
-data_fake_manual_testing.head(10)
-data_true_manual_testing.head(10)   
-
-#Merging the manual testing data in single dataset 
-data_merge = pd.concat([data_fake , data_true], axis = 0) 
-
-#Merge the true and fake news data into a single dataframe
-data = data_merge.drop(["title", "subject","date"], axis = 1)
-data.isnull().sum()
-
-#Randomly shuffle the dataframes so that the model does not learn the order of the data
-data = data.sample(frac = 1)
-data.reset_index(inplace = True)
-data.drop(["index"], axis = 1, inplace = True)
-
 #Function to preprocess the text
 #Below shown special characters are removed from the text
 def wordopt(text):
@@ -57,70 +18,16 @@ def wordopt(text):
     text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
     text = re.sub('\n', '', text)
     text = re.sub('\w*\d\w*', '', text)
-
     return text
 
-data['text'] = data['text'].apply(wordopt)
-
-#Defining dependent and independent variables
-x = data['text']        #Independent variable
-y = data['class']       #Dependent variable
-
-#Splitting the data into training and testing sets
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25)
-
-#Convert text into vectors using TF-IDF
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorization = TfidfVectorizer()
-xv_train = vectorization.fit_transform(x_train)
-xv_test = vectorization.transform(x_test)
-
-#Logistic Regression model
-from sklearn.linear_model import LogisticRegression
-LR = LogisticRegression()
-LR.fit(xv_train, y_train)
-pred_lr = LR.predict(xv_test)
-#Accuracy score for Logistic Regression model
-LR.score(xv_test, y_test)
-#Print classification report. The classification_report function builds a text report showing the main classification metrics
-print(classification_report(y_test, pred_lr))
-
-#Decision Tree Classifier model
-from sklearn.tree import DecisionTreeClassifier
-DT = DecisionTreeClassifier()
-DT.fit(xv_train, y_train)
-pred_dt = DT.predict(xv_test)
-#Accuracy score for Decision Tree model
-DT.score(xv_test, y_test)
-#Print classification report. 
-print(classification_report(y_test, pred_dt))
-
-#Gradient Boosting Classifier model
-from sklearn.ensemble import GradientBoostingClassifier
-GB = GradientBoostingClassifier(random_state=0)
-GB.fit(xv_train, y_train)
-pred_gb = GB.predict(xv_test)
-#Accuracy score for Gradient Boosting Classifier model
-GB.score(xv_test, y_test)
-#Print classification report. 
-print(classification_report(y_test, pred_gb))
-
-#Random Forest Classifier model
-from sklearn.ensemble import RandomForestClassifier
-RF = RandomForestClassifier(random_state = 0)
-RF.fit(xv_train, y_train)
-pred_rf = RF.predict(xv_test)
-#Accuracy score for Random Forest Classifier model
-RF.score(xv_test, y_test) 
-#Print classification report.
-print(classification_report(y_test, pred_rf))  
-
+#Function to return the output label
 def output_label(dataset):
     if dataset == 0:
         return "Fake News"
     elif dataset == 1:
         return "True News"
     
+#Function to manually test the news
 def manual_testing(news):
     testing_news = {"text":[news]}
     new_def_test = pd.DataFrame(testing_news)
@@ -135,6 +42,98 @@ def manual_testing(news):
                                                                                                             output_label(pred_DT[0]), 
                                                                                                             output_label(pred_GB[0]), 
                                                                                                             output_label(pred_RF[0])))
+# import the fake and real news datasets
+data_fake = pd.read_csv('NEWS/Fake.csv')
+data_true = pd.read_csv('NEWS/True.csv')
+
+#Returns the first 5 rows of the dataframe
+data_fake.head()
+data_true.head()
+
+# Add a target class column to indicate whether the news is real or fake
+data_fake["class"] = 0 
+data_true["class"] = 1
+
+#Remove last 10 rows for manual testing 
+data_fake_manual_testing = data_fake.tail(10)
+#shape() was used to get the number of rows and columns in data_fake and data_true
+for i in range(23480,23470,-1):
+    data_fake.drop([i], axis = 0, inplace = True)
+
+data_true_manual_testing = data_true.tail(10)
+for i in range(21416,21406,-1):
+    data_true.drop([i], axis = 0, inplace = True)
+
+data_fake_manual_testing["class"] = 0
+data_true_manual_testing["class"] = 1
+
+#Merging the fake and true news data in single dataset 
+data_merge = pd.concat([data_fake , data_true], axis = 0) 
+data = data_merge.drop(["title", "subject","date"], axis = 1)
+data.isnull().sum()
+
+#Randomly shuffle the 'data' so that the model does not learn the order of the data
+data = data.sample(frac = 1)
+data.reset_index(inplace = True)
+data.drop(["index"], axis = 1, inplace = True)
+
+#Preprocessing the text 
+data['text'] = data['text'].apply(wordopt)
+
+#Feature and lables
+x = data['text']    #feature of the dataset    
+y = data['class']   #label of the dataset   
+
+#Splitting the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25)
+
+#Convert text into vectors using TF-IDF
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorization = TfidfVectorizer()
+xvect_train = vectorization.fit_transform(x_train)
+xvect_test = vectorization.transform(x_test)
+
+#Logistic Regression model
+from sklearn.linear_model import LogisticRegression
+LR = LogisticRegression()
+LR.fit(xvect_train, y_train) #xvect_train is the feature matrix of the training set and y_train is the corresponding vector of labels
+pred_lr = LR.predict(xvect_test)
+#Accuracy score for Logistic Regression model
+LR.score(xvect_test, y_test)
+#Print classification report. The classification_report function builds a text report showing the main classification metrics
+print(classification_report(y_test, pred_lr))
+
+#Decision Tree Classifier model
+from sklearn.tree import DecisionTreeClassifier
+DT = DecisionTreeClassifier()
+DT.fit(xvect_train, y_train)
+pred_dt = DT.predict(xvect_test)
+#Accuracy score for Decision Tree model
+DT.score(xvect_test, y_test)
+#Print classification report. 
+print(classification_report(y_test, pred_dt))
+
+#Gradient Boosting Classifier model
+from sklearn.ensemble import GradientBoostingClassifier
+GB = GradientBoostingClassifier(random_state=0)
+GB.fit(xvect_train, y_train)
+pred_gb = GB.predict(xvect_test)
+#Accuracy score for Gradient Boosting Classifier model
+GB.score(xvect_test, y_test)
+#Print classification report. 
+print(classification_report(y_test, pred_gb))
+
+#Random Forest Classifier model
+from sklearn.ensemble import RandomForestClassifier
+RF = RandomForestClassifier(random_state = 0)
+RF.fit(xvect_train, y_train)
+pred_rf = RF.predict(xvect_test)
+#Accuracy score for Random Forest Classifier model
+RF.score(xvect_test, y_test) 
+#Print classification report.
+print(classification_report(y_test, pred_rf))  
+
+#Recieving the news from user  
 print("Enter the news text")
 news = str(input())
 manual_testing(news)
